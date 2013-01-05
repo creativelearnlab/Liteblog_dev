368b64d6277c85cc0cde7cec344fb6e68fa040a7
Xiaolong Jia <creativelearnlab@gmail.com>
2013-01-05 15:16:59 -0500

    Update liteblog/settings.py

--- liteblog/settings.py
+++ liteblog/settings.py
@@ -27,12 +27,12 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'liteblogdb',                      # Or path to database file if using sqlite3.
-        'USER': 'liteblog',                      # Not used with sqlite3.
-        'PASSWORD': 'chenjiajc',                  # Not used with sqlite3.
-        'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '5432',                      # Set to empty string for default. Not used with sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'liteblog',                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
 
